3f60c83c0d26a8a85b96cf6e8d3debbe5a122149
mlced <cedric.ngoupande.edu@groupe-gema.com>
2022-01-30 13:40:11 +0100

    backend maj

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const path = require('path');
+//mongodb+srv://m001-student:<password>@sandbox.bawe0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const app = express();
+const cors=require("cors");
+mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@sandbox.bawe0.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+module.exports = app;
